9695400dfb37dc21c57f90d20d0ce8b7677a07ee
Muhammad Talha <bsem-f18-206@superior.edu.pk>
2021-12-26 21:40:35 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import './screens/products_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
+      ),
+      title: 'Shop App',
+      home: ProductsOverviewScreen(),
+    );
+  }
+}
